23a7931d0af89654df91bd3e801b8c9a06d32207
RezaulAlamOni <rzalam93@gmail.com>
2020-10-14 02:53:50 +0600

    admin action done

---
+++ ConfigServiceAccountKey.js
@@ -0,0 +1,20 @@
+
+function returnJson() {
+  var json = {
+    "type": "service_account",
+    "project_id": "nubae-c1b63",
+    "private_key_id": "811e8e38074f1efdcf56ba00ff076a39eef92bb4",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCvbXVMPoySN5k0\nIk3XsoWO4pNbbQcaNCFRcLatg19yiAI5frz5ggbpY5M3rM6bdyU3vTBHBG+wS8cy\nAf+qj3Xt7T1yNdAojnaXxF5DqAkdyo3fE9lWqO24qafD8YGW5l+JV8etKk+fAdBf\ngtTcI6L/6EiwzgLLCuEVbeDsrjwUiuUmRlOQH0V2XARpQ68EqUdiZuoeTRnaN2qW\nFiVgKkOD0BmMhFP1t4LyczSkOUfGt3fEq5npdf4+3t2Jhbz01gQ8KM4aGBoW6lTS\nEhUTMCYVHy1SsKdhISKvA/OB1r1T+OevNmjF3p/y/RqkcTpAqt3l/rQ7ECCuo3jt\nqPE3nN7DAgMBAAECggEAPoqa8WIxUmWQ8HW0Y8xI8j5Tw5n0mMJyCGtTLTvzWM0l\nYeMNszRIi4dM8mHgzKsm8R65KfBqcyMo8mqk/+UiaX8VU3Tc78COhh/jbWJoUZ8n\nghq89rjF+3o8jc45YZdjAv9w+AroFOucIpjtk0f/MhXZaHMhqsqHoVCpiie50qZ0\nyuqPrHFMkGHrZgyLG0j0d3bPQNyEkGz642VVZ4tHEnSLrI1Xg5Kz7j/bPW9MJClQ\nbRKwWEWiNGCLaOgyPLqd7CYc/ln5q/sk5tebJyO0fK9b8umE+3AwNONpoH5/PIcc\nDRfBztXGnoUQkZt4Uk8m0GroqsF2FF5l2SmArY4olQKBgQDaqOpNiY7MXuXqtLXY\nPb9N41rm/aq7nViP4JQVqB7cjthoRKhYUsUbXaDgTeQZ9DJW2bqMGIU8rrkMoVCJ\nyFO/ajlvv0q3F4lCrLAk3K/0ftMYA/TilZUW1k1sPWuaoMORPyYS3NCzZy31OBhV\nKe+IoWRyts8PxUCAOvb5dVPwLQKBgQDNYpJbd3gLdiI5G5kNMiAhmsDtrBV4VP3N\nyAcwShG3rbQJ0kxYGhEYNJLwZEON3VRTeT9H6NZjiu9eXBuMaCSPJ0yMj1RaLB+O\nV2fdst4Nvj9SlJgmO/1BeUXFmJOGdhwGzpnjZPmM49yGAWcths26ajQud2fqSQz1\nUTQJo8hwrwKBgQCeynZE0rWR8wSS2qMII2dO/+Gex+56Cv0qlxPTWmD3GQFNTzbV\n1V3wzqCq48XXl0ALVFilrpTriZM2+dsMBB/VbSXC4i83IW97rP7J9j4NlyBjW1Q2\nTByWFhDpnv0KsN71Ft/maNdiwXZdk1wQVMDv/wx8Rge+0ydROADtDehkDQKBgQC3\nLaohP+1mM4eUbYPU3BcCQba+g5Nmjk8LQNMAg59Htfn0JIcIsL78Wq+PDa2ekn9K\nL8J7zTOg9mJFQ1sioCOzNba1SoROJDpExroRZh1cHoj1iygkOa7HvXS4Jju1WhPn\nEk74G6/is7uUHR474ZYNI/nEU6TvtRWEmlcSHeyAawKBgCwfJV2xpui1IuxUyMBL\nuznlqv8lIUJB+pYz5059rxNzZLlf1tMdzPaUarC275Tl8v2yHDxnSr1vfe1SPzRZ\ndebeTVVu7aNeXmvYFnsh9P3wyxTxZo4U+CfrTMu0hSkK/5cDVFJoYabMN8YiQOIw\no8KkzeifuDfokmSTL/2EX1RS\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-conh0@nubae-c1b63.iam.gserviceaccount.com",
+    "client_id": "108242425827053777844",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-conh0%40nubae-c1b63.iam.gserviceaccount.com"
+  }
+  return json;
+}
+
+export default returnJson();
+
+
